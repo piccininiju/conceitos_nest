4c7fecf7ec663b603f52e293dc3af806be38f314
piccinini.ju <juliana.piccinini@somagrupo.com.br>
2026-02-06 14:25:00 -0300

    entities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecadosModule } from '../recados/recados.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RecadosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '172.24.0.1',
+      port: 5432,
+      username: 'postgres',
+      database: 'postgres',
+      password: '1234',
+      autoLoadEntities: true, //carrega entidades sem precisar especifica-las
+      synchronize: true, //sincroniza com o BD, não deve ser usado em prod
+    }),
+    RecadosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
